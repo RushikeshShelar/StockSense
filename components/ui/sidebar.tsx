d09a3890f016aab4734e88433de55cdb5563ca8c
Rushi <rushikeshshelar.cs@gmail.com>
2024-02-07 23:32:17 +0530

    page: Sidbar

--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -1,77 +1,97 @@
 "use client";
 
-import { cn } from "@/lib/utils";
-import { HomeIcon, LineChartIcon, Package2Icon, PackageIcon, SettingsIcon, UserIcon } from "lucide-react";
+import { BarChart, Contact, HomeIcon, LayoutDashboard, Newspaper, Phone, Settings } from "lucide-react";
+import Logo from "./logo";
+import Money from "./money";
 import Link from "next/link";
+import { cn } from "@/lib/utils";
 import { usePathname } from "next/navigation";
-import Image from "next/image";
 
 const routes = [
     {
         label: "Home",
-        href: "/dashboard",
-        icon: HomeIcon
+        icon: HomeIcon,
+        href: "/"
     },
     {
-        label: "Stocks",
-        href: "/stocks",
-        icon: LineChartIcon
+        label: "Dashboard",
+        icon: LayoutDashboard,
+        href: "/dashboard"
     },
     {
         label: "Predictions",
-        href: "/predictions",
-        icon: PackageIcon
-    },
-    {
-        label: "Account",
-        href: "/account",
-        icon: UserIcon
-    },
+        icon: BarChart,
+        href: "/predictions"
+    }, {
+        label: "News",
+        icon: Newspaper,
+        href: "/news"
+    }
+]
+
+const supportLinks = [
     {
         label: "Settings",
-        href: "/settings",
-        icon: SettingsIcon
+        icon: Settings,
+        href: "/settings"
+    },
+    {
+        label: "Contact",
+        icon: Phone,
+        href: "/contact"
     }
-
 ]
 
 const Sidebar = () => {
-    const pathname = usePathname();
-    return (
 
-        <div>
-            <div className="px-3 py-2 flex-1">
-                <Link href="/dashboard" className="flex items-center pl-3">
-                    <div className="mr-2 text-white">
-                        <LineChartIcon className="h-6 w-6" />
-                    </div>
-                    <h1 className={cn("text-2xl font-medium text-white")}>
-                        StockSense
-                    </h1>
-                </Link>
-                <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
-                    <div className="px-3 py-2 flex-1">
-                        <div className="space-y-1">
-                            {routes.map((route) => (
-                                <Link
-                                    onClick={() => { }}
-                                    key={route.href}
-                                    href={route.href}
-                                    className={cn(
-                                        "text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition",
-                                        pathname === route.href ? "text-white bg-white/10" : "text-zinc-400",
-                                    )}
-                                >
-                                    <div className="flex items-center flex-1">
-                                        <route.icon className={cn("h-5 w-5 mr-3")} />
-                                        {route.label}
-                                    </div>
-                                </Link>
-                            ))}
-                        </div>
-                    </div>
-                </div>
+    const pathname = usePathname();
+
+    return (
+        <div className="h-full w-full relative">
+            <div className="flex flex-col">
+                <Logo />
             </div>
+            <div className="m-2 mx-4">
+                <Money amount={3000.79} />
+            </div>
+
+            <div className="flex flex-col mt-2 items-center space-y-1 mx-3">
+                {routes.map((route) => (
+                    <Link
+                        key={route.href}
+                        href={route.href}
+                        className={cn(
+                            "text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-black hover:bg-blue-900/20 rounded-lg transition",
+                            pathname === route.href ? "text-black bg-blue-900/20" : "text-gray-600",
+                        )}
+                    >
+                        <div className="flex items-center flex-1">
+                            <route.icon className="h-5 w-5 mr-3" />
+                            {route.label}
+                        </div>
+                    </Link>
+                ))}
+            </div>
+            <hr className=" mt-64" />
+
+            <div className="flex flex-col mt-2 items-center space-y-1 mx-3">
+                {supportLinks.map((route) => (
+                    <Link
+                        key={route.href}
+                        href={route.href}
+                        className={cn(
+                            "text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-black hover:bg-blue-900/20 rounded-lg transition",
+                            pathname === route.href ? "text-black bg-blue-900/20" : "text-gray-600",
+                        )}
+                    >
+                        <div className="flex items-center flex-1">
+                            <route.icon className="h-5 w-5 mr-3" />
+                            {route.label}
+                        </div>
+                    </Link>
+                ))}
+            </div>
+
         </div>
     );
 }
