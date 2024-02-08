da8c212a282e0cab51f6208f4d82be79927ba698
Rushi <rushikeshshelar.cs@gmail.com>
2024-02-08 23:53:59 +0530

    feat: Navbar

--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -1,9 +1,29 @@
+import { BellIcon, Mail, } from "lucide-react";
+import { Input } from "./input";
+import { UserButton, auth } from "@clerk/nextjs";
+
 const Navbar = () => {
-    return ( 
+
+    const { user } = auth();
+
+    return (
         <div className="h-full w-full relative">
-            Navbar
+            <div className="h-full w-full flex justify-center items-center p-4" >
+                <div className="flex w-full justify-between border-r-2 border-gray-500 pr-4">
+                    <div className="w-2/3 ">
+                        <Input className=" w-full h-8" placeholder="Press 'K' to search for stocks" />
+                    </div>
+                    <div className="flex justify-between items-center">
+                        <Mail className="h-5 w-5 mr-2 hover:cursor-pointer"/>
+                        <BellIcon className="h-5 w-5 mr-2 hover:cursor-pointer" />
+                    </div>
+                </div>
+                <div className="pl-4 flex justify-end">
+                    <UserButton afterSignOutUrl="/"/>
+                </div>
+            </div>
         </div>
-     );
+    );
 }
- 
+
 export default Navbar;
