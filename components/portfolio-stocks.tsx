ef42a21682ad441423d692515a20511a999308bb
Rushi <rushikeshshelar.cs@gmail.com>
2024-02-08 23:58:20 +0530

    temp: Portfolio-Stocks

--- components/portfolio-stocks.tsx
+++ components/portfolio-stocks.tsx
@@ -1,7 +1,32 @@
+import { Card, CardContent, CardHeader } from "./ui/card";
+
+const stocks = [
+    {
+        symbol: "AAPL",
+        name: "Apple",
+        totalShares: 10,
+        totalReturns: -10.09,
+        img: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.macworld.com%2Farticle%2F356055%2Fapple-stock-split-2020.html&psig=AOvVaw",
+    },
+]
+
 const PortfolioStocks = () => {
+    const stock = stocks[0];
     return ( 
-        <div>
-            Portfolio Stocks
+        <div className="h-full w-full relative p-4">
+            <div className="w-max hover:shadow-md transform">
+                <Card className="">
+                    <CardHeader className="p-1">
+                        <h2>{stock.symbol}</h2>
+                    </CardHeader>
+                    <CardContent>
+                        <div>
+                            <div>Total Shares: <span>${stock.totalShares}</span></div>
+                            <div>Total Returns: <span>${stock.totalReturns}</span></div>
+                        </div>
+                    </CardContent>
+                </Card>
+            </div>
         </div>
      );
 }
